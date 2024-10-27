aef10cf962f6809c82b7db158b52ab1051678859
josephtsang2012 <113126349+josephtsang2012@users.noreply.github.com>
2024-10-27 20:31:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-# import streamlit as st
-# from ui import build_ui
-
-# # Defining page settings
-# ## Ref: https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
-# st.set_page_config(
-#     page_title="Portfolio Manager",
-#     page_icon=":heavy_dollar_sign:", 
-#     layout='wide',
-#     initial_sidebar_state='auto'
-# )
-
-# # Setting theme
-# st.markdown("""
-#     <style>
-#         :root {
-#             --primary-color: #7792E3;
-#             --background-color: #FFFFFF;
-#             --secondary-background-color: #F0F2F6;
-#             --text-color: #31333F;
-#             --font: sans-serif;
-#         }
-#     </style>
-#     """, unsafe_allow_html=True)
-
-# # Building user interface
-# build_ui()
-
 import streamlit as st
 from ui import build_ui
 
@@ -38,7 +10,7 @@ st.set_page_config(
     initial_sidebar_state='auto'
 )
 
-# Set custom theme
+# Setting custom theme
 st.markdown("""
     <style>
         :root {
@@ -51,5 +23,5 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Build the UI
+# Build user interface
 build_ui()
