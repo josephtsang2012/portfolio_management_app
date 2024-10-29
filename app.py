d2f8ffea5af157f598e931c3d8f574004fa4cfbf
josephtsang2012 <113126349+josephtsang2012@users.noreply.github.com>
2024-10-29 14:44:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
-# import streamlit as st
-# from ui import build_ui
-
-# # Ref: https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
-# # Defining page settings
-# st.set_page_config(
-#     page_title="Portfolio Manager",
-#     page_icon=":heavy_dollar_sign:",
-#     layout='wide',
-#     initial_sidebar_state='auto'
-# )
-
-# # Setting custom theme
-# st.markdown("""
-#     <style>
-#         :root {
-#             --primary-color: #7792E3;
-#             --background-color: #FFFFFF;
-#             --secondary-background-color: #F0F2F6;
-#             --text-color: #31333F;
-#             --font: sans-serif;
-#         }
-#     </style>
-#     """, unsafe_allow_html=True)
-
-# # Build user interface
-# build_ui()
-
-
-
 import streamlit as st
 from ui import build_ui
 
+# Ref: https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
 # Defining page settings
 st.set_page_config(
-    page_title="PortfolioPro",
-    page_icon="💰",
+    page_title="Portfolio Manager",
+    page_icon=":heavy_dollar_sign:",
     layout='wide',
-    initial_sidebar_state='expanded'
+    initial_sidebar_state='auto'
 )
 
-# Set custom theme
+# Setting custom theme
 st.markdown("""
     <style>
         :root {
@@ -52,5 +23,34 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Build the UI
+# Build user interface
 build_ui()
+
+
+
+# import streamlit as st
+# from ui import build_ui
+
+# # Defining page settings
+# st.set_page_config(
+#     page_title="PortfolioPro",
+#     page_icon="💰",
+#     layout='wide',
+#     initial_sidebar_state='expanded'
+# )
+
+# # Set custom theme
+# st.markdown("""
+#     <style>
+#         :root {
+#             --primary-color: #7792E3;
+#             --background-color: #FFFFFF;
+#             --secondary-background-color: #F0F2F6;
+#             --text-color: #31333F;
+#             --font: sans-serif;
+#         }
+#     </style>
+#     """, unsafe_allow_html=True)
+
+# # Build the UI
+# build_ui()
