fb53d15eb8d6277a99e9613283a08ccaed30e63d
josephtsang2012 <113126349+josephtsang2012@users.noreply.github.com>
2024-10-29 13:46:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
+# import streamlit as st
+# from ui import build_ui
+
+# # Ref: https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
+# # Defining page settings
+# st.set_page_config(
+#     page_title="Portfolio Manager",
+#     page_icon=":heavy_dollar_sign:",
+#     layout='wide',
+#     initial_sidebar_state='auto'
+# )
+
+# # Setting custom theme
+# st.markdown("""
+#     <style>
+#         :root {
+#             --primary-color: #7792E3;
+#             --background-color: #FFFFFF;
+#             --secondary-background-color: #F0F2F6;
+#             --text-color: #31333F;
+#             --font: sans-serif;
+#         }
+#     </style>
+#     """, unsafe_allow_html=True)
+
+# # Build user interface
+# build_ui()
+
+
+
 import streamlit as st
 from ui import build_ui
 
-# Ref: https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
 # Defining page settings
 st.set_page_config(
-    page_title="Portfolio Manager",
-    page_icon=":heavy_dollar_sign:",
+    page_title="PortfolioPro",
+    page_icon="💰",
     layout='wide',
-    initial_sidebar_state='auto'
+    initial_sidebar_state='expanded'
 )
 
-# Setting custom theme
+# Set custom theme
 st.markdown("""
     <style>
         :root {
@@ -23,5 +52,5 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Build user interface
+# Build the UI
 build_ui()
