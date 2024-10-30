68396daa84eaadce665533779c9e937547ab11ac
josephtsang2012 <113126349+josephtsang2012@users.noreply.github.com>
2024-10-31 00:49:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,32 +25,3 @@ st.markdown("""
 
 # Build user interface
 build_ui()
-
-
-
-# import streamlit as st
-# from ui import build_ui
-
-# # Defining page settings
-# st.set_page_config(
-#     page_title="PortfolioPro",
-#     page_icon="💰",
-#     layout='wide',
-#     initial_sidebar_state='expanded'
-# )
-
-# # Set custom theme
-# st.markdown("""
-#     <style>
-#         :root {
-#             --primary-color: #7792E3;
-#             --background-color: #FFFFFF;
-#             --secondary-background-color: #F0F2F6;
-#             --text-color: #31333F;
-#             --font: sans-serif;
-#         }
-#     </style>
-#     """, unsafe_allow_html=True)
-
-# # Build the UI
-# build_ui()
